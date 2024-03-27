1a8169eda66349cf7f79939ad3bc232b0ceaedb5
Guilherme Rodrigues <guilherme.rodrigues@ait.ac.at>
2024-03-27 09:57:24 +0000

    Fixed cv_bridge include minor fix to info output
    Signed-off-by: Guilherme Rodrigues <guilherme.rodrigues@ait.ac.at>

--- bytetrack_viewer/include/bytetrack_viewer/bytetrack_viewer.hpp
+++ bytetrack_viewer/include/bytetrack_viewer/bytetrack_viewer.hpp
@@ -7,7 +7,7 @@
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/exact_time.h>
 #include <message_filters/sync_policies/approximate_time.h>
-#include <cv_bridge/cv_bridge.h>
+#include <cv_bridge/cv_bridge.hpp>
 #include <opencv2/opencv.hpp>
 
 #include "bboxes_ex_msgs/msg/bounding_box.hpp"
